b775859602ba62f072f402f2b3cded7f85912739
m0x0799 <94001698+m0x0799@users.noreply.github.com>
2022-11-19 00:37:16 +0100

    Update UARTcmd.py

--- UARTcmd.py
+++ UARTcmd.py
@@ -1,7 +1,8 @@
 import serial
 
 def sendCMD(cmd:str):
-    with serial.Serial(/dev/ttyAMA0, 115200, timeout=1) as myser:
+    with serial.Serial('/dev/ttyAMA0', 115200, timeout=1) as myser:
         myser.write(cmd.encode())
         asw = myser.read(20)
         return asw
+    
